1c11f0901f6fd0e47d21bc48f4795d8b883a869b
Chii Law <cyee3012@hotmail.co.uk>
2021-04-01 16:16:58 +0900

    defined variable to increase diameter size

--- dot-game/assets/public/script.js
+++ dot-game/assets/public/script.js
@@ -1,4 +1,6 @@
 const dot = document.querySelector('.dot');
+const plusDiameter = Math.floor(Math.random() * 40);
+const diameter = (dot + plusDiameter);
 
 dot.addEventListener('click', () => {
   dot.style.display = 'none';
