23325ffd29d3a8b96784490f2b5cfa792d141c39
Chii Law <cyee3012@hotmail.co.uk>
2021-04-01 16:35:02 +0900

    dot chances to random size between 10-50px when page reloads

--- dot-game/assets/public/script.js
+++ dot-game/assets/public/script.js
@@ -1,6 +1,10 @@
 const dot = document.querySelector('.dot');
-const plusDiameter = Math.floor(Math.random() * 40);
-const diameter = (dot + plusDiameter);
+
+const newDiameter = 10 + Math.floor(Math.random() * 40);
+
+dot.style.width = `${newDiameter}px`;
+dot.style.height = dot.style.width;
+
 
 dot.addEventListener('click', () => {
   dot.style.display = 'none';
