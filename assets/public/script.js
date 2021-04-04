c6d520bfce15e997fdb322103897ca178c810466
Chii Law <cyee3012@hotmail.co.uk>
2021-04-04 16:22:00 +0900

    multiple dots are able to move up the game. scores also accumelate properly accordly to size

--- assets/public/script.js
+++ assets/public/script.js
@@ -1,66 +1,72 @@
-let dot = document.querySelector('.dot');
-let points = document.querySelector('.points');
 const score = document.querySelector('.score');
 const speed = document.querySelector('.speed');
 const pause = document.querySelector('.pause');
 const gameContainer = document.querySelector('.game-container');
 
 // toggle speed
-speedSetting = 1;
+speedSetting = 30;
 speed.addEventListener('click', () => {
   if (speed.innerText == "speed: 4x") {
   speed.innerText = "speed: 1x"
-  speedSetting = 1;
+  speedSetting = 30;
   } else if ( speed.innerText == "speed: 1x") {
     speed.innerText = "speed: 2x"
-    speedSetting = 2;
+    speedSetting = 60;
   } else if (speed.innerText == "speed: 2x") {
     speed.innerText = "speed: 4x"
-    speedSetting = 4;
+    speedSetting = 120;
   }
 });
 
-// makes dot random size between 10-50px;
-const movingDot = (dot) => {
-  let diameter = 10 + Math.floor(Math.random() * 41);
-  let xPosition = Math.floor(Math.random() * (301 - (diameter)));
-  let yPosition = Math.floor(Math.random() * (1 - (diameter)));
+const createDot = () => {
+  // create actual dot
+  const dot = document.createElement("div");
+  dot.classList.add('dot');
+  dot.dataset.points = 'points'
+  gameContainer.appendChild(dot);
+
+  const diameter = 10 + Math.floor(Math.random() * 41);
+  const xPosition = Math.floor(Math.random() * (301 - (diameter)));
+  const yPosition = (0 - diameter) / 2;
   console.log(yPosition)
   dot.style.display = 'block';
   dot.style.width = `${diameter}px`;
   dot.style.height = dot.style.width;
   dot.style.left = `${xPosition}px`;
   dot.style.bottom = `${yPosition}px`;
-  console.log(xPosition);
-  console.log(diameter);
+  console.log(`${xPosition} xPosition` );
+  console.log(`${diameter} diameter`);
+  // add points element to dot
+  // makes points inversely proportional to the dot size (range from 1-10 points)
+  // -1px = +0.225 points. 50 - diameter = pointIncrement * 0.225 (+1 to include 50px and 10 px)
+  pointsAmount = 1 + (50 - parseFloat(dot.style.width)) * 0.225;
+  dot.dataset.points = `${Math.round(pointsAmount)}`;
+  console.log(`${dot.dataset.points} points`);
+  return dot
+};
 
   // dot moves from bottom to top
-  position = yPosition;
-    const moveDot = setInterval(function() {
-    newPositon = position += speedSetting;
+const moveDot = (dot) => {
+  let newPositon = parseFloat(dot.style.bottom);
+  const dotMovement = setInterval(function() {
+    console.log(newPositon);
+    newPositon = newPositon + speedSetting;
     dot.style.bottom = newPositon +"px";
-  }, 5000);
-
-  //makes points inversely proportional to the dot size (range from 1-10 points)
-  // -1px = +0.225 points. 50 - diameter = pointIncrement * 0.225 (+1 to include 50px and 10 px)
-  pointsAmount = 1 + (50 - diameter) * 0.225;
-  points.innerText = `${Math.round(pointsAmount)}`;
-  console.log(points.innerText);
-
-  // click to make dot disappear
-  dot.addEventListener('click', () => {
-    dot.style.display = 'none';
-    score.innerText = `${parseInt(score.innerText)+parseInt(points.innerText)}`;
-    dot.style.bottom = 0;
-    clearInterval(moveDot);
-  });
+  }, 1000);
+// click to make dot disappear
+dot.addEventListener('click', () => {
+  dot.style.display = 'none';
+  score.innerText = `${parseInt(score.innerText)+parseInt(dot.dataset.points)}`;
+  dot.style.bottom = 0;
+  clearInterval(dotMovement);
+});
+  return dot
 };
+
 
 // // duplicate dot
 const dotsAppear = setInterval(function() {
-  let moreDots = dot.cloneNode(true);
-  newDot = gameContainer.appendChild(moreDots);
-  movingDot(newDot);
+  moveDot(createDot());
 }, 1000);
 
 
@@ -73,11 +79,11 @@ pause.addEventListener('click', () => {
   } else if (pause.innerText == "resume?") {
     pause.innerText = "pause";
       if (speed.innerText == "speed: 4x") {
-       speedSetting = 4;
+       speedSetting = 120;
       } else if (speed.innerText == "speed: 2x") {
-        speedSetting = 2;
+        speedSetting = 60;
       } else if (speed.innerText == "speed: 1x") {
-        speedSetting = 1;
+        speedSetting = 30;
       };
     };
 });
